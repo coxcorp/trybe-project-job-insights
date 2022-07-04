f386315352f079eea4381535fa5decc223f1f225
Diego Silva <coxcorp@gmail.com>
2022-07-04 13:42:20 -0300

    requisito 05

--- src/insights.py
+++ src/insights.py
@@ -65,21 +65,13 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    complete_document = read(path)
+    salaries = list()
+    for row in complete_document:
+        if row["min_salary"].isdigit():
+            new_salary = int(row["min_salary"])
+            salaries.append(new_salary)
+    return min(salaries)
 
 
 def matches_salary_range(job, salary):
